530cbb5d5f30c6204b28747858ef50f743fc45cd
Samantha Campisi <samantha@creativehothouse.com>
2020-05-22 11:33:25 +0200

    Changes project name to userManagementTest

--- ios/userManagementTestTests/userManagementTestTests.m
+++ ios/userManagementTestTests/userManagementTestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TypescriptReactNativeStarterTests : XCTestCase
+@interface userManagementTestTests : XCTestCase
 
 @end
 
-@implementation TypescriptReactNativeStarterTests
+@implementation userManagementTestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
